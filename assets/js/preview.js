8e5637b16790246411eef90beb15c424ab08f849
pyliu <pangyu.liu@gmail.com>
2018-10-05 10:21:06 +0800

    adjust UI

--- assets/js/preview.js
+++ assets/js/preview.js
@@ -6,48 +6,94 @@ $(document).ready(function(e) {
             json[$.trim(this.name)] = $.trim(this.value);
         });
         console.log(json);
+    });
+    /*
+    $("#preview").on("click", function(e) {
 
+        var current_opts = $("#inheritance_form").serializeArray();
+        var json = {};
+        $.each(current_opts, function() {
+            json[$.trim(this.name)] = $.trim(this.value);
+        });
+        console.log(json);
         var html = "";
+        var tree_data = [];
         // #0 收件資訊
         if (!isEmpty(json["serial"]) && !isEmpty(json["heir"])) {
-            html += "<p><h4>#0 收件資訊</h4>";
-            html += "<div>收件年期字號：" + json["serial"] + "</div>";
-            html += "<div>被繼承人姓名：" + json["heir"] + "</div>";
-            html += "</p>";
+            tree_data.push({
+                text: "#0 收件資訊",
+                nodes: [{
+                    text: "收件年期字號："+ json["serial"]
+                }, {
+                    text: "被繼承人姓名：" + json["heir"]
+                }]
+            });
         }
 
         if (!isEmpty(json["heir_reg_type"])) {
-            html += "<p><h4>#1 繼承登記類型</h4>";
+            var text = "";
             switch (json["heir_reg_type"]) {
                 case "type_0_law_heir":
-                    html += "<div>法定繼承</div>";
+                    text += "法定繼承";
                     break;
                 case "type_1_split_heir":
-                    html += "<div>分割繼承</div>";
+                    text += "分割繼承";
                     break;
                 case "type_2_share_heir":
-                    html += "<div>共同共有繼承</div>";
+                    text += "共同共有繼承";
                     break;
                 case "type_3_modify":
-                    html += "<div>名義更正</div>";
+                    text += "名義更正";
                     break;
                 case "type_4_will_heir":
-                    html += "<div>遺囑繼承</div>";
+                    text += "遺囑繼承";
                     break;
                 case "type_5_judge_heir":
-                    html += "<div>判決繼承</div>";
+                    text += "判決繼承";
                     break;
                 case "type_6_nobody_heir":
-                    html += "<div>無人承認繼承</div>";
+                    text += "無人承認繼承";
                     break;    
             }
-            html += "</p>";
+            tree_data.push({
+                text: "#1 繼承登記類型",
+                nodes: [{
+                    text: text
+                }]
+            });
         }
 
         if (!isEmpty(json["death_period"])) {
-            html += "<p><h4>#2 被繼承人審查項目</h4>";
+            function getNodes(var_name) {
+                var nodes = [];
+                if (var_name == "death_period") {
+                    if (!isEmpty(json["house_owner"])) {
+                        if (json["house_owner"] == "yes") {
+                            nodes.push({
+                                text: "戶主",
+                                nodes: getNodes("house_owner")
+                            });
+                        } else {
+                            nodes.push({
+                                text: "非戶主",
+                                nodes: getNodes("house_owner")
+                            });
+                        }
+                    }
+                }
+                return nodes;
+            }
+            tree_data.push({
+                text: "#2 被繼承人審查項目",
+                nodes: [{
+                    text: json["death_period"] == "jp" ? "日據時期(民國34年10月24日前死亡)" : "光復後(民國34年10月25日後死亡)",
+                    nodes: getNodes("death_period")
+                }]
+            });
         }
 
-        $("#preview").html(html);
+        $("#tree").treeview({data: tree_data});
+
     });
+    */
 });
